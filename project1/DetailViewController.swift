36766104b3f900fee51f66a2de186a71c7ddd4ea
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-04-04 13:42:49 +0545

    chore: image view added

---
+++ project1/DetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by Waterflow Technology on 04/04/2025.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    //@IBOutlet tells xcode that the line of code has connection to something on interface builder.
+    @IBOutlet var imageView: UIImageView!
+    //implicitly unwrapped UIImageView as it might be nil and it will be there when we need it in this case.
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad);
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
