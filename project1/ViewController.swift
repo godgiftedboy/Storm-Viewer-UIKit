4054d96dd18fde0211163e8ce2a6be330468163f
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-04-04 13:53:21 +0545

    chore: added title and arrow forward

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        
         // Do any additional setup after loading the view.
         let fm = FileManager.default;
         let path = Bundle.main.resourcePath!
