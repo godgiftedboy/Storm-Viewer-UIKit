dae97e6b596c77db5551a9ef0896936cc89a26d6
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-07 12:55:43 +0545

    chore: share button on navbar and allow save to device by editin info plist file

--- project1/DetailViewController.swift
+++ project1/DetailViewController.swift
@@ -44,7 +44,12 @@ class DetailViewController: UIViewController {
     //so @objc is required in this function as it is being accessed by UIBarButtonItem.
     //so that it can recognize the function.
     @objc func shareTapped (){
-        let vc = UIActivityViewController(activityItems: [], applicationActivities: []);
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: []);
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem;
         present(vc, animated: true);
     }
